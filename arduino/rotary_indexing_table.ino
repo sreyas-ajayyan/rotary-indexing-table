c4f2b284798681ed6f1013966622fe65f6318b53
Sreyas Ajayyan <sreyasajayyan2003@gmail.com>
2025-07-27 01:14:54 +0530

    Update rotary_indexing_table.ino

--- arduino/rotary_indexing_table.ino
+++ arduino/rotary_indexing_table.ino
@@ -1,1 +1,16 @@
-
+#include <Stepper.h>
+const int stepsPerRevolution = 2048;
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+void setup() 
+{
+  myStepper.setSpeed(10);
+}
+void loop() 
+{
+  for (int i = 0; i < 4; i++) 
+  {
+    myStepper.step(stepsPerRevolution / 4);
+    delay(2000);
+  }
+ delay(4000);
+}
